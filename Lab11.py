213fab170f3fda5d25aab0bb5dd42b46e67bf9c4
ananyaa-sutaria <ananyaa.sutaria@gmail.com>
2024-11-20 09:46:10 -0500

    Add files via upload

---
+++ Lab11.py
@@ -0,0 +1,12 @@
+def main():
+
+    print('1. Student grade')
+    print('Assignment statistics')
+    print('Assignment graph')
+
+    selection = int(input('Enter your selection: '))
+
+    if selection == 1:
+        name = input("What is the student's name: ")
+    if selection == 2:
+        assignment = input('What is the assignment name: ')
